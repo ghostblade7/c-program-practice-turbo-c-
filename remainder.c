9aedcbe08be1642b215f2d07eea38dcfe50f7f61
ghostblade7 <rockysencr7@gmail.com>
2026-08-20 19:17:00 +0530

    Create remainder.c

---
+++ remainder.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+    int a,b,c;
+
+    clrscr();
+
+    printf("Enter your first number: ");
+    scanf("%d",&a);
+
+    printf("Enter your second number: ");
+    scanf("%d",&b);
+
+    c=a%b;
+
+    printf("\nRemainder = %d",c);
+
+    getch();
+}
