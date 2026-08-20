6266607d8d1a11fa98286ba44b470c5cbb57eac4
ghostblade7 <rockysencr7@gmail.com>
2026-08-20 19:15:08 +0530

    Update sum.c

--- sum.c
+++ sum.c
@@ -4,11 +4,12 @@
 void main()
 {
   int a,b,c;
+  clrscr();
   printf("Enter your first number");
   scanf("%d",&a);
   printf("enter your second nunber");
   scanf("%d",&b);
   c=a+b;
-  printf("Sum of the two number is %d",c);
+  printf("\n Sum of the two number is %d",c);
     getch();
 }
