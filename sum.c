ed6b4a429eaa27f255cac5010267462fbef41cbf
ghostblade7 <rockysencr7@gmail.com>
2026-08-20 19:11:55 +0530

    Create sum.c

---
+++ sum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+
+void main()
+{
+  int a,b,c;
+  printf("Enter your first number");
+  scanf("%d",&a);
+  printf("enter your second nunber");
+  scanf("%d",&b);
+  c=a+b;
+  printf("Sum of the two number is %d",c);
+    getch();
+}
